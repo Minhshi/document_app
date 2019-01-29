cf18483ff6630ce04e6cbeb8f5466a4563c36d5a
Minh Pham <mpham@naver.com>
2019-01-29 13:18:58 +0100

    Clean schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_064817) do
+ActiveRecord::Schema.define(version: 2019_01_29_120829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "attachments", force: :cascade do |t|
-    t.string "photo"
-    t.bigint "message_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["message_id"], name: "index_attachments_on_message_id"
-  end
 
   create_table "message_attachments", force: :cascade do |t|
     t.string "attachment"
@@ -55,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_01_29_064817) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "attachments", "messages"
   add_foreign_key "message_attachments", "messages"
   add_foreign_key "messages", "users"
 end
