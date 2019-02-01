5c6527545e592b1a4a8c85acebd4db39f7fc5391
Minh Pham <mpham@naver.com>
2019-02-01 19:54:30 +0100

    Change email from address to default due to error in postmark

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -8,7 +8,12 @@ class MessageMailer < ApplicationMailer
   def message_email(message_content)
     @message = message_content
     # attachments.inline['photo.png'] = File.read('path/to/photo.png')
-    mail(from:@message.user.email, to: "mpham@naver.com", subject: @message.topic)
+    # mail(from:@message.user.email, to: "mpham@naver.com", subject: @message.topic)
+    # # mail.attachments['logo.png'] = File.read('app/assets/images/logo.png')
+    # mail.attachments['attachment.pdf'] = WickedPdf.new.pdf_from_string(
+    #   render_to_string(:pdf => "show",:template => 'messages/show.pdf.erb')
+    #   )
+    mail(to: "mpham@naver.com", subject: @message.topic)
     # mail.attachments['logo.png'] = File.read('app/assets/images/logo.png')
     mail.attachments['attachment.pdf'] = WickedPdf.new.pdf_from_string(
       render_to_string(:pdf => "show",:template => 'messages/show.pdf.erb')
